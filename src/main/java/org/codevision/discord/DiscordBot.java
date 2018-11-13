12aff49b5d70f0967d59979abb7393ccd31da424
Tom <tomhazredstone@hotmail.com>
2018-11-13 10:46:55 -0600

    First commit. Gradle and main class.

---
+++ src/main/java/org/codevision/discord/DiscordBot.java
@@ -0,0 +1,12 @@
+package org.codevision.discord;
+
+public final class DiscordBot {
+
+    private DiscordBot() {
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Hello, world!");
+    }
+
+}
